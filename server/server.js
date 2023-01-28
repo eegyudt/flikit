8058add5108226d913be7d3bcf3350591fbe9c44
Amina Malik <amina.malik@telus.com>
2023-01-27 22:12:01 -0500

    setup server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 54321;
+
+
+// database?
+
+
+//routes
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+
+
+});
